699b5a46ae36557c5899eb96d47ccba50a1ae493
Dep-lux <163655761+Dep-lux@users.noreply.github.com>
2024-11-07 16:23:58 +0900

    2nd

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 $(document).ready(function () {
     $(".backimg img:first-child").css("z-index", "-1").siblings().hide();
     $(".nav-menu__html").hover(function () {
-        $(".html_img").stop(true, true).fadeIn(500).siblings().delay(100).fadeOut(500);
+        $(".html_img").stop(true, true).fadeIn(300).siblings().delay(200).fadeOut(300);
     });
     $(".nav-menu__css").hover(function () {
-        $(".css_img").stop(true, true).fadeIn(500).siblings().delay(100).fadeOut(500);
+        $(".css_img").stop(true, true).fadeIn(300).siblings().delay(200).fadeOut(300);
     });
     $(".nav-menu__javascript").hover(function () {
-        $(".js_img").stop(true, true).fadeIn(500).siblings().delay(100).fadeOut(500);
+        $(".js_img").stop(true, true).fadeIn(300).siblings().delay(200).fadeOut(300);
     });
     $(".nav-menu__home").hover(function () {
-        $(".home_img").stop(true, true).fadeIn(500).siblings().delay(100).fadeOut(500);
+        $(".home_img").stop(true, true).fadeIn(300).siblings().delay(200).fadeOut(300);
     });
+
+    setInterval(function () {
+        if ($(".nav-menu li:last-child").hasClass("active")) {
+            $(".nav-menu li.active").removeClass("active").delay(400).siblings("li:first-child").addClass("active");
+        } else {
+            $(".nav-menu li.active").removeClass("active").delay(400).next().addClass("active");
+        }
+    }, 5000);
 });
