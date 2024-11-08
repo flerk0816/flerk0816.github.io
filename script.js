10f6111377ba03a0e06f15fd08a9b9d9735959d2
Dep-lux <163655761+Dep-lux@users.noreply.github.com>
2024-11-08 11:17:01 +0900

    3rd

--- script.js
+++ script.js
@@ -19,5 +19,5 @@ $(document).ready(function () {
         } else {
             $(".nav-menu li.active").removeClass("active").delay(400).next().addClass("active");
         }
-    }, 5000);
+    }, 3000);
 });
